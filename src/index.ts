e107561a10f0b842cfb7d09f3735e7fbcdcf95b6
NeoTRAN001 <neo_tran@outlook.com>
2024-02-10 17:14:51 -0600

    feat: define commands

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import inquirer from "inquirer";
+import { program } from "commander";
 import { banner } from "./ui/main";
 
-console.clear();
-console.log(banner);
+program
+  .name('lithium-util')
+  .description(banner)
+  .version('0.0.1');
+
+
+program
+  .command("app:build")
+  .description("Build an application")
+  .option("-o, --output <output-dir>", "Specify the output directory");
+
+program
+  .command("app:create")
+  .description("Create an applications from a scaffolding")
+  .option("-n, --name <app-name>", "Specify the application name");
+
+
+program
+  .command("app:serve")
+  .description("Serve an application")
+  .option("-p, --port", "Specifies the server port");
+
+
+program.parse();
